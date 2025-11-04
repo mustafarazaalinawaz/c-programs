d3e25e20d4132c0e0566235bd76330b43e2a2622
mustafarazaalinawaz <mustafarazaalinawaz@gmail.com>
2025-11-04 21:15:35 +0530

    Add files via upload

---
+++ HELLO WORLD USING STRINGS.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    
+    char s[100];
+    char message[] = "Hello, World!";
+    char welcome_message[] = "Welcome to C programming.";
+    
+    printf("%s\n", message);
+     printf("%s\n", welcome_message);
+
+      
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
